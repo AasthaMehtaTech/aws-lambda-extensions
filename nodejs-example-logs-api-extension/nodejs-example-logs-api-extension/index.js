0f44adf6dbbc4697de2171d6bc4a183fc0372a72
AasthaTechO <aasthamehta2704@gmail.com>
2023-07-26 14:40:37 +0530

    chore: add console logs to figure out ES_Index the logs are being sent to

--- nodejs-example-logs-api-extension/nodejs-example-logs-api-extension/index.js
+++ nodejs-example-logs-api-extension/nodejs-example-logs-api-extension/index.js
@@ -36,8 +36,8 @@ const RECEIVER_NAME = 'sandbox';
 async function receiverAddress() {
   return process.env.AWS_SAM_LOCAL === 'true' ? LOCAL_DEBUGGING_IP : RECEIVER_NAME;
 }
-
-const ELASTICSEARCH_ENDPOINT = process.env.ELASTICSEARCH_ADD_DOC_ENDPOINT || `https://${process.env.ES_ENDPOINT}/my-index/_doc`; // Replace with your Elasticsearch insertion endpoint along with <index>/_doc
+const ES_INDEX = `lambda-${process.env.AWS_LAMBDA_FUNCTION_NAME}`;
+const ELASTICSEARCH_ENDPOINT = process.env.ELASTICSEARCH_ADD_DOC_ENDPOINT || `https://${process.env.ES_ENDPOINT}/${ES_INDEX}/_doc`; // Replace with your Elasticsearch insertion endpoint along with <index>/_doc
 
 const RECEIVER_PORT = 4243;
 const TIMEOUT_MS = 1000; // Maximum time (in milliseconds) that a batch is buffered.
@@ -101,7 +101,7 @@ const SUBSCRIPTION_BODY = {
 
                 req.write(postData);
                 req.end();
-                console.log('DEBUG: log processing & es exporting complete')
+                console.log('DEBUG: log processing & es exporting complete to endpoint:', ELASTICSEARCH_ENDPOINT);
 
             } catch (error) {
                 console.error(`[${this.agent_name}] Error: ${error}`, flush = true);
