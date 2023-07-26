619cd9cddce37cceef545abd3c00a3931d76ff4f
AasthaTechO <aasthamehta2704@gmail.com>
2023-07-26 19:03:07 +0530

    feat: add records processing logic

--- nodejs-example-logs-api-extension/nodejs-example-logs-api-extension/http-listener.js
+++ nodejs-example-logs-api-extension/nodejs-example-logs-api-extension/http-listener.js
@@ -1,5 +1,26 @@
 const http = require('http');
 
+function processBatch(batch) {
+    const processedBatch = batch
+      .filter((item) => item.type === "function")
+      .map((item) => {
+        const recordParts = item.record.split("\t");
+        const time = recordParts[0];
+        const requestId = recordParts[1];
+        const level = recordParts[2];
+        const data = JSON.parse(recordParts[3]);
+  
+        return {
+          time,
+          requestId,
+          level,
+          ...data,
+        };
+      });
+  
+    return processedBatch;
+  }
+
 function listen(address, port) {
     const logsQueue = [];
     // init HTTP server for the Logs API subscription
@@ -14,9 +35,12 @@ function listen(address, port) {
                 try {
                     let batch = JSON.parse(body);
                     console.log('DEBUG body:', body);
-                    console.log('DEBUG batch:', batch);
-                    if (batch.length > 0) {
-                        logsQueue.push( ...batch );
+                    // console.log('DEBUG batch:', batch);
+                    const processedBatch = processBatch(batch);
+                    console.log('DEBUG processedBatch:', processedBatch);
+
+                    if (processedBatch.length > 0) {
+                        logsQueue.push( ...processedBatch );
                     }
                 } catch(e) { 
                     console.log("failed to parse logs"); 
